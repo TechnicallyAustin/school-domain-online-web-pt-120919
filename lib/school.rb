3efe3503d6b701c37ddacf4ea70b1bf45aaf8251
Austin Johnson <technicallyaustin@gmail.com>
2020-01-10 01:13:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+  attr_accessor :grade_level, :name
+  attr_reader :school, :roster
+  def initialize(school)
+    roster = {}
+    @school = school
+    @roster = roster
+  end
+
+  def add_student(name, grade)
+    #adds a single student as a hash value with their grade level as a key 
+    student_array = []
+    student_array << self.name
+    @student_array = student_array
+    @roster[grade_level] = @student_array 
+    
+    
+    
+    
+  end
+  
+  
+end
